5801755b4b29253ec53b2eae0472ea7bb4516cbf
Matthew Saxe <mattsaxe17@gmail.com>
2021-09-30 22:38:56 -0400

    Add loader.io config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./api/routes');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -16,6 +17,10 @@ app.use(bodyParser.json());
 // })
 
 app.use('/qa', routes);
+
+app.get('/loaderio-82e1afa4c08267caa569c0851a3b132f', (req, res) => {
+  res.sendFile(path.resolve('./loader.io.txt'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log('App listening on port ', process.env.PORT)
