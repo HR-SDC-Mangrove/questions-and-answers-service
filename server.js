5dde058c04cc08929841254d07fdcc41c3551a1d
Matthew Saxe <mattsaxe17@gmail.com>
2021-10-03 08:52:36 -0400

    Chang eport config

--- server.js
+++ server.js
@@ -9,19 +9,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Req logger
-// app.use((req, res, next) => {
-//   console.log('BODY', req.body);
-//   console.log('PARAMS', req.query);
-//   next();
-// })
-
 app.use('/qa', routes);
 
 app.get('/loaderio-82e1afa4c08267caa569c0851a3b132f', (req, res) => {
   res.sendFile(path.resolve('./loader.io.txt'));
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('App listening on port ', process.env.PORT)
+app.listen(process.env.NODE_ENV === 'production' ? 80 : process.env.PORT, () => {
+    console.log('App listening on port ', process.env.NODE_ENV === 'production' ? 80 : process.env.PORT)
 });
